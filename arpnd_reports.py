fae7799c7dd5469e7691a38d944f9f68303e8fb1
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-03-23 20:59:44 -0500

    Existing code does it like this

--- arpnd_reports.py
+++ arpnd_reports.py
@@ -149,9 +149,9 @@ class Plugin(CliPlugin):
          for c in i.forwarding_complex.items():
           for x in c.datapath.get().xdp.get().resource.items():
             # TODO average or collect all
-            self._used_percent = x.used_percent
-            self._used_entries = x.used_entries
-            self._free_entries = x.free_entries
+            self._used_percent = x.used_percent if x.used_percent is not None else '-'
+            self._used_entries = x.used_entries if x.used_entries is not None else '-'
+            self._free_entries = x.free_entries if x.free_entries is not None else '-'
 
         cap = data.capacity.create()
         cap.used_percent = self._used_percent
